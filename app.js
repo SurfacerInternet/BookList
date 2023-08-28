ba24ef979c7c1ebf7d2aa585e293b163ff8e7ae2
Mohammad Siddiqui <sidqmohammad@gmail.com>
2023-08-28 09:49:43 -0400

    Show book object in console.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Book Class
+// Book Class: Represents a Book
 class Book {
     constructor(title, author, isbn) {
       this.title = title;
@@ -6,39 +6,62 @@ class Book {
       this.isbn = isbn;
     }
   }
-
-
-//UI class
-class UI {
+  
+  // UI Class: Handle UI Tasks
+  class UI {
     static displayBooks() {
-        const StoredBooks = [
+      const StoredBooks = [
         {
-            title: "Harry Potter: Goblet of Fire",
-            author: "JK Rowling",
-            isbn: '3434434'
-        }, 
+          title: 'Harry Potter',
+          author: 'JK Rowling',
+          isbn: '3434434'
+        },
         {
-            title: "Of Mice and Men",
-            author: "John Stienback",
-            isbn: '455445'
+          title: 'Of Mice and Men',
+          author: 'John Steinback',
+          isbn: '45545'
         }
-        ];
-        const books = StoredBooks;
-        books.forEach((book) => UI.addBookToList(book));
-    }
+      ];
 
+  
+      const books = StoredBooks;
+  
+      books.forEach((book) => UI.addBookToList(book));
+    }
+  
     static addBookToList(book) {
-        const list = document.querySelector('#book-list');
-        const row= document.createElement('tr');
-        row.innerHTML = `
+      const list = document.querySelector('#book-list');
+  
+      const row = document.createElement('tr');
+  //creating a row, by first grabbing onto an element. 
+      row.innerHTML = `
         <td>${book.title}</td>
         <td>${book.author}</td>
         <td>${book.isbn}</td>
-        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td> 
-        `;
-        list.appendChild(row);
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+      `;
+  
+      list.appendChild(row);
     }
-}
-//Display Books
-document.addEventListener('DOMContentLoaded', UI.displayBooks);
-//Event: Add a Book 
+  
+  }
+ 
+  // Event: Display Books
+  document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#book-form').addEventListener('submit', (e) => 
+  {
+    //Prevent real submit
+    e.preventDefault();
+    // Get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+  
+
+    //instatiate book
+    const book = new Book (title, author, isbn);
+
+    console.log(book)
+  })
+})
+   
