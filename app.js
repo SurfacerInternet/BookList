e325e18d201e3fecdd58098747181902dc9e04c7
Mohammad Siddiqui <sidqmohammad@gmail.com>
2023-08-28 14:29:40 -0400

    Remove book from list

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ class Book {
       const list = document.querySelector('#book-list');
   
       const row = document.createElement('tr');
-  //creating a row, by first grabbing onto an element. 
+ 
       row.innerHTML = `
         <td>${book.title}</td>
         <td>${book.author}</td>
@@ -43,25 +43,51 @@ class Book {
   
       list.appendChild(row);
     }
+
+    static deleteBook(el) { 
+      if(el.classList.contains('delete')) {
+        el.parentElement.parentElement.remove();
+      }
+    }
   
+    static clearFields() {
+      document.querySelector('#title').value='';
+      document.querySelector('#author').value='';
+      document.querySelector('#isbn').value=''
+    }
+
   }
  
   // Event: Display Books
-  document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#book-form').addEventListener('submit', (e) => 
+document.addEventListener("DOMContentLoaded", UI.displayBooks);
+
+document.addEventListener('DOMContentLoaded', () => {
+document.querySelector('#book-form').addEventListener('submit', (e) => 
   {
-    //Prevent real submit
+//Prevent real submit
     e.preventDefault();
     // Get form values
     const title = document.querySelector('#title').value;
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
   
-
     //instatiate book
     const book = new Book (title, author, isbn);
 
-    console.log(book)
-  })
-})
-   
+    //Add books to UI
+    UI.addBookToList(book)
+
+    //Clear Fields
+    UI.clearFields();
+  ;
+  });
+}
+)
+
+
+//Event: Remove Book
+
+document.addEventListener( "DOMContentLoaded", ()=> document.querySelector('#book-list').addEventListener('click', (e) => {
+    // Remove book from UI
+    UI.deleteBook(e.target)
+}))
